cd965266db8ba3dfc2f230f2773c0661e6b091d0
Beleza2 <do_nelis@hotmail.com>
2026-06-30 12:08:34 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener("scroll", () => {
+  document.querySelectorAll("section").forEach(section => {
+    const top = section.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      section.classList.add("show");
+    }
+  });
+});
