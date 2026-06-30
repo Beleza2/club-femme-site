9f5894a06fde97603c31bfb198cafeeefcd2533b
Beleza2 <do_nelis@hotmail.com>
2026-06-30 12:07:37 -0300

    Delete assets/images/script.js

--- assets/images/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", () => {
-  document.querySelectorAll("section").forEach(section => {
-    const top = section.getBoundingClientRect().top;
-    if (top < window.innerHeight - 100) {
-      section.classList.add("show");
-    }
-  });
-});
